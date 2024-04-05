934125895fd285d0da2f5f66f4ed0ee1a14abd60
Aiza Farooq <72245945+aiza411@users.noreply.github.com>
2024-04-05 15:32:26 +0500

    Done

---
+++ Day9.js
@@ -0,0 +1,23 @@
+//alert("Hello Aiza");
+
+//var keyword
+//Can be redeclared and updated. Global Scope.
+var age=22;
+var age=20;
+var age=21;
+console.log("Age: ",age);
+
+//let keyword
+//Can't be redeclared but can be updated. Block level Scope.
+let FullName="Aiza";
+ //let FullName="Aiza";
+FullName="Aiza Farooq";
+console.log("Hello",FullName);
+
+
+//const keyword
+//Can't be redeclared and updated. Block level Scope.
+
+const money=1000;
+//money=1900;
+console.log("Money",money);
