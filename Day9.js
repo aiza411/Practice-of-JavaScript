4ebec138e3195191d51accdd8f312563580276cd
Aiza Farooq <72245945+aiza411@users.noreply.github.com>
2024-04-05 17:38:34 +0500

    Product Description

--- Day9.js
+++ Day9.js
@@ -1,23 +1,47 @@
-//alert("Hello Aiza");
+const product={
+    productName:"Parker Jotter Standard CT Ball Pen",
+    productDesciption:"Highly professional and reliable. An ideal partner with unlimited potential, Parker Jotter is all at once smart, polished and established. With a durable stainless steel nib and finishes that echo the Parker legacy.",
+    color:"Black",
+    rating:4,
+    price:270,
+};
 
-//var keyword
-//Can be redeclared and updated. Global Scope.
-var age=22;
-var age=20;
-var age=21;
-console.log("Age: ",age);
+document.getElementById("ProductName").textContent=product.productName;
+document.getElementById("Color").textContent='('+product.color+')';
+document.getElementById("ProductDescription").textContent=product.productDesciption;
+document.getElementById("ProductPrice").textContent="Rs : "+product.price;
+AddRatingStars();
 
-//let keyword
-//Can't be redeclared but can be updated. Block level Scope.
-let FullName="Aiza";
- //let FullName="Aiza";
-FullName="Aiza Farooq";
-console.log("Hello",FullName);
+function AddRatingStars()
+{
+    for(var i=0 ; i<5 ; i++)
+    {
+        if(i<product.rating)
+        {
+            var img=document.createElement('img');
+            img.src="/Practice-of-JavaScript/assets/starFull.png";
+            img.classList.add("image-fluid");
+            img.style.padding="2px";
+            document.getElementById("ProductRating").appendChild(img);
+        }
 
+        else{
+            var img=document.createElement('img');
+            img.src="/Practice-of-JavaScript/assets/starEmpty.png";
+            img.style.padding="2px";
+            img.classList.add("image-fluid");
+            document.getElementById("ProductRating").appendChild(img);
+        }
+    }
+}
 
-//const keyword
-//Can't be redeclared and updated. Block level Scope.
+function BuyItem()
+{
+    alert("Item Purchased Successfully");
+}
 
-const money=1000;
-//money=1900;
-console.log("Money",money);
+document.getElementById("BuyBtn").addEventListener('click',function(){
+    alert("Item Purchased Successfully");
+});
+
+console.log(product);
